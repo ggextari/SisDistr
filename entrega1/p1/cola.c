339f88933d3200457075aeb97824397a6356a799
ggextari <uo266457@uniovi.es>
2024-03-31 16:02:15 +0000

    Primera prueba de código

--- entrega1/p1/cola.c
+++ entrega1/p1/cola.c
@@ -21,11 +21,32 @@ void inicializar_cola(Cola *cola, int tam_cola)
     }
 
     // A RELLENAR el resto de la inicialización de la cola
-    // mutex_init y sem_init
-    |
-    |
-    |
-    |
+    // Inicialización de la cola
+    cola->datos = (dato_cola **)malloc(sizeof(dato_cola *) * tam_cola);
+    if (cola->datos == NULL)
+    {
+        perror("Error: No se pudo reservar memoria para la cola");
+        exit(3);
+    }
+    cola->tam = tam_cola;
+    cola->primero = cola->ultimo = 0;
+
+    // Inicialización de mutex y semáforos
+    if (pthread_mutex_init(&(cola->mutex), NULL) != 0)
+    {
+        perror("Error: No se pudo inicializar el mutex de la cola");
+        exit(4);
+    }
+    if (sem_init(&(cola->lleno), 0, 0) != 0)
+    {
+        perror("Error: No se pudo inicializar el semáforo de cola llena");
+        exit(5);
+    }
+    if (sem_init(&(cola->vacio), 0, tam_cola) != 0)
+    {
+        perror("Error: No se pudo inicializar el semáforo de cola vacía");
+        exit(6);
+    }
 }
 
 void destruir_cola(Cola *cola)
@@ -35,22 +56,35 @@ void destruir_cola(Cola *cola)
     // destruir los semáforos y mutexes
     
     // A RELLENAR
-    |
-    |
-    |
-    |
-    |
+    // Liberar la memoria de cada elemento de la cola
+    for (int i = 0; i < cola->tam; i++)
+    {
+        if (cola->datos[i] != NULL)
+        {
+            free(cola->datos[i]);
+            cola->datos[i] = NULL;
+        }
+    }
 
+    // Liberar la memoria de la cola y destruir mutex y semáforos
+    free(cola->datos);
+    pthread_mutex_destroy(&(cola->mutex));
+    sem_destroy(&(cola->lleno));
+    sem_destroy(&(cola->vacio));
 }
 
 void insertar_dato_cola(Cola *cola, dato_cola *dato)
 {
     // A RELLENAR
-    |
-    |
-    |
-    |
+    sem_wait(&(cola->vacio)); // Esperar a que haya espacio en la cola
+    pthread_mutex_lock(&(cola->mutex)); // Bloquear el acceso a la cola
 
+    // Insertar el dato en la cola
+    cola->datos[cola->ultimo] = dato;
+    cola->ultimo = (cola->ultimo + 1) % cola->tam;
+
+    pthread_mutex_unlock(&(cola->mutex)); // Desbloquear el acceso a la cola
+    sem_post(&(cola->lleno)); // Incrementar el contador de elementos en la cola
 }
 
 dato_cola *obtener_dato_cola(Cola *cola)
@@ -58,8 +92,16 @@ dato_cola *obtener_dato_cola(Cola *cola)
     dato_cola *p;
 
     // A RELLENAR
-    |
-    |
-    |
-    |
+    sem_wait(&(cola->lleno)); // Esperar a que haya elementos en la cola
+    pthread_mutex_lock(&(cola->mutex)); // Bloquear el acceso a la cola
+
+    // Obtener el dato de la cola
+    p = cola->datos[cola->primero];
+    cola->primero = (cola->primero + 1) % cola->tam;
+
+    pthread_mutex_unlock(&(cola->mutex)); // Desbloquear el acceso a la cola
+    sem_post(&(cola->vacio)); // Incrementar el contador de espacios libres en la cola
+
+    return p;
 }
+
