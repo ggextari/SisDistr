fd20d96f5f27f33269bba9d39643e0b378906f14
ggextari <uo266457@uniovi.es>
2024-04-17 08:53:52 +0000

    Fin clase

--- rabbit/ejemplo2/NewTask.java
+++ rabbit/ejemplo2/NewTask.java
@@ -6,21 +6,22 @@ public class NewTask {
 
   private final static String QUEUE_NAME = "hello";
 
-  //private static String getMessage(String[] strings)
-  //{
-  //   if (strings.length < 1)
-  //      return "Hello World!";
-  //}
-  private static String joinStrings(String[] strings, String delimiter)
-  {
-     int length = strings.length;
-     if (length == 0) return "";
-     StringBuilder words = new StringBuilder(strings[0]);
-     for (int i = 1; i < length; i++) {
-        words.append(delimiter).append(strings[i]);
-     }
-     return words.toString();
+  // private static String getMessage(String[] strings)
+  // {
+  // if (strings.length < 1)
+  // return "Hello World!";
+  // }
+  private static String joinStrings(String[] strings, String delimiter) {
+    int length = strings.length;
+    if (length == 0)
+      return "";
+    StringBuilder words = new StringBuilder(strings[0]);
+    for (int i = 1; i < length; i++) {
+      words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
   }
+
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
