b3f95b988afc59a704893d9ce3508705dfbb27ca
ggextari <uo266457@uniovi.es>
2024-06-07 15:21:18 +0000

    Terminada p2.1

--- examen/p2.1/prodcons.c
+++ examen/p2.1/prodcons.c
@@ -19,35 +19,57 @@ el que recibe debe mostrarlo por su salida standard.
 
 #define BUFFER_SIZE 1024
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    int fds[2];
+    pipe(fds);  /* Crea un pipe */
 
     if (argc != 2) {
-        printf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+        fprintf(stderr, "Uso %s <nombre_del_fichero>\n", argv[0]);
     }
 
-    int fds[2];
-    pipe(fds);
-
+    /*Este proceso deberá enviar el fichero al otro proceso a través del pipe */
     if (fork() == 0)
     {
-        dup2(fds[0],0);
-        close(fds[1]);
-        while ( (count = read(fd, buffer, 100)))
+        close(fds[0]); //Cierra el extremo de lectura del pipe
+        
+        int fd = open(argv[1], O_RDONLY);
+        if (fd < 0) {
+            perror("Error al abrir el fichero");
+            exit(EXIT_FAILURE);
+        }
+
+        char buffer[BUFFER_SIZE];
+        ssize_t bytesRead;
+        while((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
+            write(fds[1], buffer, bytesRead);
+        }
+
+        close(fd);
+        close(fds[1]); // Cierra el extremo de escritura del pipe
+        exit(EXIT_SUCCESS);
     }
     else if (fork() == 0)
     {
-        dup2(fds[1],1);
-        close(fds[0]);
-        FILE *f = fopen(argv[1], "r");
-        execlp()
+        close(fds[1]); // Cierra el extremo de escritura del pipe
+        
+        char buffer[BUFFER_SIZE];
+        ssize_t bytesRead;
+        while ((bytesRead = read(fds[0], buffer, BUFFER_SIZE)) > 0) {
+            write(STDOUT_FILENO, buffer, bytesRead);
+        }
+
+        close(fds[0]);  // Cierra el extremo de lectura del pipe
+        exit(EXIT_SUCCESS);
     }
+    /* El padre cierra ambos descriptores del pipe y espera la finalización de sus hijos */
     else
     {
-        close(fds[0]);
+        close(fds[0]);  // Cierra ambos extremos del pipe en el proceso padres
         close(fds[1]);
-        wait(0);
+        wait(0);  // Espera la finalización de ambos hijos
         wait(0);
     }
+
+    return 0;
 }
